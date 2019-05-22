c96b1f10a0d77734fd76d5602c203a0f7834bd9d
中川　大地 <195x113x@istc-a-049.epc.center.kobe-u.ac.jp>
2019-05-22 15:04:11 +0900

    ititial

---
+++ W09/Task03.js
@@ -0,0 +1,36 @@
+function main03()
+{
+    var volume = new KVS.LobsterData();
+    //var screen = new KVS.THREEScreen();
+    var material = new THREE.ShaderMaterial({
+vertexColors: THREE.VertexColors,
+vertexShader: document.getElementById('gouraud.vert').text,
+fragmentShader: document.getElementById('gouraud.frag').text,
+uniforms: {
+light_position: { type: 'v3', value: light.position }
+}
+});
+
+    screen.init( volume, {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        enableAutoResize: false
+    });
+
+    var bounds = Bounds( volume );
+    screen.scene.add( bounds );
+
+    var isovalue = 128;
+    var surfaces = Isosurfaces( volume, isovalue );
+    screen.scene.add( surfaces );
+
+    document.addEventListener( 'mousemove', function() {
+        screen.light.position.copy( screen.camera.position );
+    });
+
+    window.addEventListener( 'resize', function() {
+        screen.resize( [ window.innerWidth, window.innerHeight ] );
+    });
+
+    screen.loop();
+}
